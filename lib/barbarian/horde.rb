8b99f7fd73cbdea704dca1c1ad2ea3e738d8be20
Frederick Cheung <frederick.cheung@gmail.com>
2013-05-07 19:28:42 +0100

    simplest possible thread based runner

--- lib/barbarian/horde.rb
+++ lib/barbarian/horde.rb
@@ -1,5 +1,29 @@
+require 'thread'
 module Barbarian
   class Horde
-    #TODO: make this do something!
+    def initialize(klass, count)
+      @count = count
+      @klass = klass
+      @event_queue = Queue.new
+    end
+
+    def run
+      @running = true
+      @threads = 
+      @count.times.collect do |index|
+        sleep 0.5
+        Thread.new do |t|
+          while @running
+            @klass.new(index).run
+            @event_queue.push :kind => :run_completed
+          end
+        end
+      end
+    end
+
+    def stop
+      @running = false
+      @threads.each(&:join)
+    end
   end
 end
